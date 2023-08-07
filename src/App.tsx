5f759319b0eeaa407dc430404b58c4d829cbb125
Pavel M <april504@yandex.ru>
2023-08-07 19:18:38 +0700

    feat: dark theme support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useMemo } from "react";
+
 import "./App.css";
 import Counters from "./components/Counters";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Counters />
-    </>
+      {/* <Routes /> */}
+    </ThemeProvider>
   );
 }
 
